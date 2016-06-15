38eedf20ff63645e5934735f9543dc8016818b88
yolixtly <yolixtly_13@hotmail.com>
2016-06-15 16:15:12 -0400

    first API request atempt

--- app/app.js
+++ app/app.js
@@ -1,21 +1,47 @@
 var app = angular.module('imgApp', ['ngAnimate']);
 
-app.controller("MemoriesCtrl", function(){
+app.controller("MemoriesCtrl",['$http', '$q', function($http, $q){
 
-	var vm = this;
+  var vm = this;
 
-	//this is false, so Results will not be shown until form is valid
-	vm.showResults = false;
+  // Store the number of photos returned by the API request. This number is display on the UI
+  vm.photosTotal = null;
 
-	vm.displayMemories = function(){
-		if(vm.searchForm.$valid) {
-			vm.showResults = true;
-		}
-	};
+//Accessing the Flicker API : 
+   var getFlickerData = function(){
+     var url = 'https://api.flickr.com/services/rest/';
+     var params = {
+      method : 'flickr.photos.search',
+      api_key : '500ecb12edc8c861d2e69492a08ed8f5',
+      tags : vm.searchTerm,
+      format : 'json',
+      nojsoncallback : 1
+    };
 
-	vm.refreshSearch = function() {
-		vm.showResults = false;
-		vm.searchTerm = "";
-		vm.searchForm.$setPristine();
-	}
-});
+    $http.get(url, params).then(function(response){
+          console.log("getFlickerData Success!");
+          console.log(response);
+          // vm.photosTotal = response.data.photos.photo;
+    },function(response){
+         console.log("error retriving the getFlickerData");
+         console.log(response);
+    });
+   };
+
+
+  //this is false, so Results will not be shown until form is valid
+  vm.showResults = false;
+
+  vm.displayMemories = function(){
+    if(vm.searchForm.$valid) {
+      vm.showResults = true;
+      getFlickerData();
+    }
+  };
+
+  vm.refreshSearch = function() {
+    vm.showResults = false;
+    vm.searchTerm = "";
+    vm.searchForm.$setPristine();
+  };
+}]);
