d483208635ff5738a3d53a9fffb5309b5803393f
yolixtly <yolixtly_13@hotmail.com>
2016-06-15 09:03:55 -0400

    first commit with 50% responsive design

---
+++ app/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('imgApp', ['ngAnimate']);
+
+app.controller("MemoriesCtrl", function(){
+
+	var vm = this;
+
+	//this is false, so Results will not be shown until form is valid
+	vm.showResults = false;
+
+	vm.displayMemories = function(){
+		if(vm.searchForm.$valid) {
+			vm.showResults = true;
+		}
+	};
+
+});
