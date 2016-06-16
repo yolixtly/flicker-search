6363b3e6afc9cf75f846da12d5dd2cf9a9513b13
yolixtly <yolixtly_13@hotmail.com>
2016-06-16 09:36:37 -0400

    image are render in UI

--- app/app.js
+++ app/app.js
@@ -1,40 +1,37 @@
 var app = angular.module('imgApp', ['ngAnimate']);
+  //helps configuring $http to make Cross-origin HTTP requests
+  //this has been tested with and without it and it works fine without. leave it for now
+app.config(function($httpProvider){
+   $httpProvider.defaults.useXDomain = true;
+});
 
 app.controller("MemoriesCtrl",['$http', '$q', function($http, $q){
 
   var vm = this;
 
-  // Store the number of photos returned by the API request. This number is display on the UI
+  //  variable to store the # of Photos found in the search.
+  // It is initially set to null, because there is not initial number.
   vm.photosTotal = null;
 
-//Accessing the Flicker API : 
-   var getFlickerData = function(){
-     var url = 'https://api.flickr.com/services/rest/';
-     var params = {
-      method : 'flickr.photos.search',
-      api_key : '500ecb12edc8c861d2e69492a08ed8f5',
-      tags : vm.searchTerm,
-      format : 'json',
-      nojsoncallback : 1
-    };
+  //error message is false, unless the Request gets an Error response
+  vm.errorRequest = false; 
 
-    $http.get(url, params).then(function(response){
-          console.log("getFlickerData Success!");
-          console.log(response);
-          // vm.photosTotal = response.data.photos.photo;
-    },function(response){
-         console.log("error retriving the getFlickerData");
-         console.log(response);
-    });
-   };
-
-
-  //this is false, so Results will not be shown until form is valid
+  //this is false, so Results Section will not be shown until form is valid
   vm.showResults = false;
 
+  //this will save an array of urls to be iterate by ng-src in the UI
+  vm.urlArray = [];
+
+ 
   vm.displayMemories = function(){
     if(vm.searchForm.$valid) {
+
+      //Renders the results section, hides the search section
       vm.showResults = true;
+
+
+
+      //Makes the REST API Request and brings with it all the goodies!
       getFlickerData();
     }
   };
@@ -44,4 +41,57 @@ app.controller("MemoriesCtrl",['$http', '$q', function($http, $q){
     vm.searchTerm = "";
     vm.searchForm.$setPristine();
   };
+
+//Accessing the Flicker API : 
+   var getFlickerData = function(){
+     var url = 'https://api.flickr.com/services/rest';
+     var params = {
+      method : 'flickr.photos.search',
+      api_key : '500ecb12edc8c861d2e69492a08ed8f5',
+      tags : vm.searchTerm,
+      format : 'json',
+      nojsoncallback : 1
+    };
+
+    $http({
+        url: url,
+        params: params,
+        method: 'GET'
+    }).then(function(response){
+          console.log("getFlickerData Success!");
+          console.log(response);
+          vm.photosTotal = response.data.photos.total;
+          
+          vm.photos = response.data.photos.photo;
+          
+    },function(response){
+         console.log("error retriving the getFlickerData");
+         console.log(response);
+         vm.errorRequest = true;
+    });
+   };
+
+
+   //creating $q promise to select all the Relevant data and place it as an image in the UI
+
+      // var createUrl = function(response){
+
+      //   return $q(function(resolve){
+
+      //     for(var i = 0; i < response.photos.photo.length; i++){
+
+      //           var farm  = response.data.photos.photo[i].farm;
+      //           var server = response.data.photos.photo[i].server;
+      //           var id = response.data.photos.photo[i].id;
+      //           var secret = response.data.photos.photo[i].secret;
+
+      //           // push each url to vm.urlArray
+      //          vm.urlArray[i] = 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '.jpg';
+
+      //         }
+      //   });
+      // };
+
+        
+  
 }]);
